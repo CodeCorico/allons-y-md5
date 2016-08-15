af02333b8b548b17b969bb02e267de17b6705c1f
Xavier Boubert <xavier@boubert.fr>
2016-08-16 00:36:52 +0200

    feat(md5): create feature

---
+++ features/md5/md5-gulpfile.js
@@ -0,0 +1,24 @@
+'use strict';
+
+module.exports = ['$gulp', function($gulp) {
+
+  var sourcemaps = require('gulp-sourcemaps'),
+      uglify = require('gulp-uglify'),
+      rename = require('gulp-rename');
+
+  $gulp.task('md5', function(done) {
+    $gulp
+      .src('node_modules/blueimp-md5/js/md5.js')
+      .pipe($gulp.dest('./public/vendor'))
+      .pipe(sourcemaps.init())
+      .pipe(uglify())
+      .pipe(rename({
+        extname: '.min.js'
+      }))
+      .pipe(sourcemaps.write('./'))
+      .pipe($gulp.dest('./public/vendor'))
+      .on('end', done);
+  });
+
+  return 'md5';
+}];
